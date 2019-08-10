d53833392ff0857d059a20fca64ca7c61f895c6e
chatchai <s14_110@hotmail.com>
2019-08-10 19:10:42 +0700

    .

--- src/com/stack/Stack.java
+++ src/com/stack/Stack.java
@@ -1,7 +1,7 @@
 package com.stack;
 
 
-class Stack {
+public class Stack {
     private static int top = 0;
     private static int[] a = new int[10];
 
@@ -52,4 +52,32 @@ class Stack {
             }
         }
     }
+}
+
+class Main {
+    public static void main(String args[]) {
+        Stack s = new Stack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        s.push(40);
+        s.push(50);
+        s.push(60);
+        s.push(70);
+        s.push(80);
+        s.push(90);
+        System.out.println("********************************");
+        s.pop();
+        s.pop();
+        s.pop();
+        s.peek();
+        System.out.println("********************************");
+        System.out.println("Stack is empty value ? ");
+        s.isEmpty();
+        System.out.println("********************************");
+        System.out.println("list Stack");
+        s.printStack();
+    }
+
+
 }
